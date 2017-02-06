f71203aa205095a19cdc14f35c2d778c79266a2f
Alexandre DAmato <rocko83a@gmail.com>
2017-02-06 10:23:12 -0200

    console java fix

--- consolejava.sh
+++ consolejava.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-ls -1tr /home/damato/Downloads/*.jnlp | tail -n 1 | xargs -i /home/damato/java/bin/javaws {}
+#!/bin/bash -x
+ls -1tr /home/damato/Downloads/*.jnlp* | tail -n 1 | xargs -i /home/damato/java/bin/javaws {}
