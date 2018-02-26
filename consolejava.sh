a6a1ad1a45fa275f5a00460c5fdf887faeeaf403
Alexandre DAmato <rocko83a@gmail.com>
2018-02-26 13:14:41 -0300

    verbosity added to the output

--- consolejava.sh
+++ consolejava.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-ls -1tr /home/damato/Downloads/*.jnlp* | tail -n 1 | xargs -i /home/damato/java/bin/javaws {}
+ls -1tr /home/damato/Downloads/*.jnlp* | tail -n 1 | xargs -i /home/damato/java/bin/javaws -verbose -wait {}
